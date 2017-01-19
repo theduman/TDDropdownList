2ad0c32f78367ede567c5335955d11956fb4bc1d
Emre <emreduman16@gmail.com@Hatice-MacBook-Pro.local>
2017-01-19 16:30:20 +0200

    fix

--- TDDropdownList/TDDropdownList.swift
+++ TDDropdownList/TDDropdownList.swift
@@ -21,7 +21,7 @@ open class TDDropdownList: UIView {
     var dropDownData: [String] = []
     var isActive = false
     var selectedItem = UIButton(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-    var delegate: TDDropdownListDelegate?
+    open var delegate: TDDropdownListDelegate?
     var animationTimer: Double = Double()
     public override init(frame: CGRect) {
         super.init(frame: frame)
